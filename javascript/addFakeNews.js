ecc3b034d0d4835db9d7c2312740202599f95b7e
demo <demo@provisions-MacBook-Air.local>
2019-06-17 12:35:58 +0800

    editted button

--- javascript/addFakeNews.js
+++ javascript/addFakeNews.js
@@ -1,1 +1,21 @@
+function writeFakeNews() {
+  $("#summary").empty();
+  let title = $("#title").value();
+  let link = $("#link1").value();
 
+  let text = "";
+  if (title != "" && link != "") {
+    text = title + " and " + link;
+  } else if (title == "") {
+    text = link;
+  } else {
+    text = title;
+  }
+
+  $("#summary").append("fake news reported: " + text);
+
+function initializeJs() {
+  $("#submitButton").click(writeFakeNews);
+}
+
+$(document).ready(initializeJs);
