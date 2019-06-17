e078108a0e538ffa4b1a9404fbd9f7b578a94394
ovidharshini <demo@provisions-MacBook-Air.local>
2019-06-17 11:51:08 +0800

    import data-model

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import data-model
+import os
+import webapp2
+import jinja2
 
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
