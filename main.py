e864bd739ba64e0279e9081c46ad4ea378afa323
LollyWanger <39096500+LollyWanger@users.noreply.github.com>
2019-06-19 11:27:31 +0800

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#from data_model import User
 import os
 import webapp2
 import jinja2
 from google.appengine.api import users
+from data_model import Post
 
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -42,28 +42,58 @@ class HomePage(webapp2.RequestHandler):
         home = the_jinja_env.get_template('templates/homepage.html')
         self.response.write(home.render())
 
+    def post(self):
+        postresults = the_jinja_env.get_template('templates/homepage.html')
+        fakenews_title = self.request.get('title')
+        fakenews_link = self.request.get('link')
+        fakenews_message = self.request.get('message')
+
+        fakenews = Post(
+            title = fakenews_title,
+            message = fakenews_message,
+            link = fakenews_link
+        )
+
+        fakenews.put()
+
+        dict = {
+            "fakeNewsLink": fakenews_link,
+            "fakeNewsMessage": fakenews_message,
+            "fakeNewsTitle": fakenews_title
+        }
+        self.response.write(postresults.render(dict))
+
+
+
+
+
+
+class AddFakeNews(webapp2.RequestHandler): #/addFakeNews
+    def get(self):
+        self.redirect("/homepage")
+    def post(self):
+        addFakeNews = the_jinja_env.get_template('templates/addFakeNews.html')
+        self.response.write(addFakeNews.render())
+
+
+
+
+
+
+
+
+
+
+
+
 
 class NewsResults(webapp2.RequestHandler):
     pass
 
-class AddFakeNews(webapp2.RequestHandler):
+class AboutUs(webapp2.RequestHandler):
     def post(self):
-        addFakeNews = the_jinja_env.get_template('templates/addFakeNews.html')
-
-        fakenews_title = self.request.get('#title')
-        fakenews_link = self.request.get('#link')
-        fakenews_message = self.request.get('message')
-
-        fakenews = data_model.Post(
-            title = fakenews_title,
-            description = fakenews_message,
-            message = fakenews_message
-        )
-
-        fakenews.put()
-        self.response.write(addFakeNews.render())
-
-
+        about = the_jinja_env.get_template('templates/aboutUs.html')
+        self.response.write(about.render())
 
 
 
@@ -76,4 +106,6 @@ app = webapp2.WSGIApplication([
     ('newsresults', NewsResults),
     ('/homepage', HomePage),
     ('/addFakeNews', AddFakeNews),
+    ('/aboutus', AboutUs),
 ], debug = True)
+
