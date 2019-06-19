0910cf1f2041e3e54608af2357e55d57c3fe4d26
LollyWanger <39096500+LollyWanger@users.noreply.github.com>
2019-06-19 15:26:29 +0800

    added datetime property

--- data_model.py
+++ data_model.py
@@ -5,4 +5,4 @@ class Post(ndb.Model):
     title = ndb.StringProperty(required=True)
     message = ndb.StringProperty(required=True)
     link = ndb.StringProperty(required = True)
-#    date = ndb.DateTimeProperty(required = True)
+    date = ndb.DateTimeProperty(required = True, auto_now_add = True)
