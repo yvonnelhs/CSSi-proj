51bb6b154f990149b0074e67325874a04ef8f0f8
LollyWanger <39096500+LollyWanger@users.noreply.github.com>
2019-06-20 02:59:36 +0800

    Update data_model.py

--- data_model.py
+++ data_model.py
@@ -6,3 +6,5 @@ class Post(ndb.Model):
     message = ndb.StringProperty(required=True)
     link = ndb.StringProperty(required = True)
     date = ndb.DateTimeProperty(required = True, auto_now_add = True)
+
+print Post.query().fetch()[0]
