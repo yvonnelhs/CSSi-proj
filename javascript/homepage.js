f7e4c3612c78066b73e5af345c0da823667d38fb
Oviyadharshini <ovidharshini@gmail.com>
2019-06-20 08:26:18 +0800

    loading is at page bottom

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,1 +1,15 @@
+var count=0;
+$(document).ready(function(){
+  $(window).scroll(function(){
+    //Check for user has reached bottom of Page
+    if($(window).scrollTop()==($(document).height()-window.innerHeight)){
+      $('#loading').fadeIn();
+      setTimeout("appendContent()", 1000);
+    }
+  });
+});
+var appendContent=function(){
+  $('#content').prepend("---------------------<br>Title: {{fakeNewsTitle}} <br>Link: {{fakeNewsLink}} <br>Message: {{fakeNewsMessage}}<br>---------------------");
 
+  $('#loading').fadeOut();
+};
